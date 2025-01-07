d256995f78f0f7ac183050d8a43bd894bdc0f613
Varun Negi <varun.py10@gmail.com>
2025-01-08 05:23:10 +0530

    Add CORS support to the Express server

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ import "dotenv/config";
 import express from "express";
 import { createServer } from "http";
 
+import cors from "cors";
 import bookRoute from "./routes/books.js";
 import transcationsRoute from "./routes/transcations.js";
 import userRoute from "./routes/user.js";
-
 const app = express();
 const port = 8000;
+const allowedOrigins = [
+  process.env.FRONTEND_URL ? process.env.FRONTEND_URL : "http://localhost:3000",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+app.use(cors(corsOptions));
 app.use(express.json());
-
 const server = createServer(app);
 
 app.get("/", (req, res) => {
